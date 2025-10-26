7b2d0386d6a31014f87903f486cdc661ee70fb64
archuser <lipikamishra89@gmail.com>
2025-10-26 11:40:39 +1100

    Micro

---
+++ hello.py
@@ -0,0 +1,12 @@
+print("Hi how are you today")
+
+answer = input("")
+
+if answer == "good":
+    print("Nice to hear that! ")
+
+elif answer == "bad":
+    print("Sorry to hear that. Hope things get better!")    
+
+else:
+    print("I mean it is a alright lil bro")
